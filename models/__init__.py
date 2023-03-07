f7657d33bdc21d0fb99fc52db06b09d54269da37
Jenaide <jenaidesibolie@gmail.com>
2023-03-07 19:46:48 +0000

    added files and folders

---
+++ models/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Created by
+@authors: Jenaide Sibolie
+"""
+from models.engine.file_storage import FileStorage
+
+
+storage = FileStorage()
+storage.reload()
---
+++ models/amenity.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Created by
+@author: Jenaide Sibolie
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Amenity class inherits from BaseModel
+    Attribute:
+        name (str): Public class attribute for Amenity's name
+    """
+    name = ""
