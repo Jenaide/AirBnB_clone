fde822b2c434e3e3f2c313e0a17bc9f40e1924f8
Jenaide <jenaidesibolie@gmail.com>
2023-03-11 20:08:41 +0000

    code fix

--- console.py
+++ console.py
@@ -28,9 +28,9 @@ class HBNBCommand(cmd.Cmd):
     def default(self, line):
         """Catch commands if nothing else matches then."""
         # print("DEF:::", line)
-        self._precmd(line
+        self.precmd(line)
 
-    def _precmd(self, line):
+    def precmd(self, line):
         """Intercepts commands to test for class.syntax()"""
         # print("PRECMD:::", line)
         match = re.search(r"^(\w*)\.(\w+)(?:\(([^)]*)\))$", line)
