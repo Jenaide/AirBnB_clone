d49b5e20eadbaa206bcb3703cd172b6e5c7dd200
Jenaide <jenaidesibolie@gmail.com>
2023-03-11 20:26:18 +0000

    code fix in filestorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,8 @@ Created on Wed March 08 22:05:43 2023
 @authors: Jenaide Sibolie
 """
 import json
+import datetime
+import os
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -19,7 +21,7 @@ class FileStorage:
     files.
     """
     __file_path = 'file.json'
-    __objects = dict()
+    __objects = {}
 
 
     def __init__(self):
@@ -50,15 +52,72 @@ class FileStorage:
         with open(FileStorage.__file_path, 'w', encoding='utf-8') as file:
             json.dumps(dictionary, file)
 
+    def classes(self):
+        """Returns a dictionary of valid classes and their references"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
+
+        classes = {"BaseModel": BaseModel,
+                   "User": User,
+                   "State": State,
+                   "City": City,
+                   "Amenity": Amenity,
+                   "Place": Place,
+                   "Review": Review}
+        return classes
+
     def reload(self):
         """deserializes the JSON file to __objects ONLY if the JASON file
         exists, otherwise, do nothing.  If the file doesn't exist, exceptions
         should be raised
         """
-        try:
-            with open(FileStorage.__file_path, 'r', encoding='utf-8') as file:
-                json_load = json.load(file)
-            for x, y in json_load.items():
-                FileStorage.__objects[x] = BaseModel(**y)
-        except:
-            pass
+        if not os.path.isfile(FileStorage.__file_path):
+            return
+        with open(FileStorage.__file_path, "r", encoding="utf-8") as file:
+            obj_dict = json.load(file)
+            obj_dict = {k: self,classes() [v["__class__"]](**v)
+                        for k, v in obj_dict.items()}
+            FileStorage.__objects = obj_dict
+
+    def attributes(self):
+        """Returns the valid attributes and their types for classname"""
+        attributes = {
+            "BaseModel":
+                     {"id": str,
+                      "created_at": datetime.datetime,
+                      "updated_at": datetime.datetime},
+            "User":
+                     {"email": str,
+                      "password": str,
+                      "first_name": str,
+                      "last_name": str},
+            "State":
+                     {"name": str},
+            "City":
+                     {"state_id": str,
+                      "name": str},
+            "Amenity":
+                     {"name": str},
+            "Place":
+                     {"city_id": str,
+                      "user_id": str,
+                      "name": str,
+                      "description": str,
+                      "number_rooms": int,
+                      "number_bathrooms": int,
+                      "max_guest": int,
+                      "price_by_night": int,
+                      "latitude": float,
+                      "longitude": float,
+                      "amenity_ids": list},
+            "Review":
+            {"place_id": str,
+                         "user_id": str,
+                         "text": str}
+        }
+        return attributes
