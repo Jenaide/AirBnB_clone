fa85da25d866ebea3c784e025b0f7e9bb669e8b0
Jenaide <jenaidesibolie@gmail.com>
2023-03-11 21:51:17 +0000

    bug fix

--- models/base_model.py
+++ models/base_model.py
@@ -34,7 +34,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
 
 
     def __str__(self):
@@ -51,7 +51,7 @@ class BaseModel:
         current datetime
         """
         self.update_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
 
     def to_dict(self):
